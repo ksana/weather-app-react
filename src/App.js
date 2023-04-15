0d256e7435f37b19cddf859562125e8c85ba3f66
Ksana <ksana@users.noreply.github.com>
2023-04-15 19:11:45 +0200

    added link to repo

--- src/App.js
+++ src/App.js
@@ -1,19 +1,19 @@
 import "./App.css";
-//import Weather from "./Weather";
-//import { Audio, Blocks } from "react-loader-spinner";
-//import Bottom from "./Bottom.js";
 import Top from "./Top.js";
-//import Middle from "./Middle";
+import CodeLink from "./CodeLink";
 import Search from "./Search";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <Top />
-        <Search />
-        {/* <Middle /> */}
-      </header>
+    <div>
+      <div className="App">
+        <header className="App-header">
+          <Top />
+          <Search />
+          {/* <Middle /> */}
+        </header>
+      </div>
+      <CodeLink />
     </div>
   );
 }
---
+++ src/CodeLink.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function CodeLink() {
+  return (
+    <div>
+      This page is coded by Oksana M, see code in
+      <a href="https://github.com/ksana/weather-app-react"> GIT </a>
+    </div>
+  );
+}
